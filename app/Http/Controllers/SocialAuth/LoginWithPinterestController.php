a1833e8760e2f34feae503ad55de8e3fec2ec1fd
DevRabiul <devrabiul@gmail.com>
2023-03-19 19:58:52 +0600

    Initial commit

---
+++ app/Http/Controllers/SocialAuth/LoginWithPinterestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\SocialAuth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginWithPinterestController extends Controller
+{
+    //
+}
